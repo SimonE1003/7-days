7e56f3d3af45855e82d93914a0aca550421a8ceb
AlonsoCltz <alonsosun2006@gmail.com>
2025-03-10 15:14:18 +0800

    map_prototype

--- map_related/map_main.cpp
+++ map_related/map_main.cpp
@@ -1,6 +1,17 @@
 #include <ncurses.h>
 #include <bits/stdc++.h>
-
+using namespace std;
+void clearnear(int x,int y){
+    mvprintw(x,y," ");
+    mvprintw(x+1,y," ");
+    mvprintw(x-1,y," ");
+    mvprintw(x,y+1," ");
+    mvprintw(x,y-1," ");
+    mvprintw(x+1,y+1," ");
+    mvprintw(x+1,y-1," ");
+    mvprintw(x-1,y+1," ");
+    mvprintw(x-1,y-1," ");
+}
 int main() {
     // Initialize ncurses
     initscr();
@@ -13,24 +24,33 @@ int main() {
     getmaxyx(stdscr, height, width);
 
     int ch;
+    int charactorpos[2] = {height / 2, width / 2};
+    mvprintw(charactorpos[0], charactorpos[1], "X");
     while ((ch = getch()) != 'q') { // Press 'q' to exit the loop
         switch (ch) {
             case KEY_UP:
                 mvprintw(0, 0, "Up arrow key pressed");
+                charactorpos[0] -= 1;
                 break;
             case KEY_DOWN:
                 mvprintw(0, 0, "Down arrow key pressed");
+                charactorpos[0] += 1;
                 break;
             case KEY_LEFT:
                 mvprintw(0, 0, "Left arrow key pressed");
+                charactorpos[1] -= 1;
                 break;
             case KEY_RIGHT:
                 mvprintw(0, 0, "Right arrow key pressed");
+                charactorpos[1] += 1;
                 break;
             default:
-                mvprintw(0, 0, "Key pressed: %c", ch);
+                mvprintw(0, 0, "Key pressed: %c       ", ch);
                 break;
         }
+        clearnear(charactorpos[0],charactorpos[1]);
+        mvprintw(charactorpos[0], charactorpos[1], "X");
+        mvprintw(1, 0, "Position: %d, %d", charactorpos[0], charactorpos[1]);
         refresh();
     }
 
@@ -39,3 +59,5 @@ int main() {
 
     return 0;
 }
+
+// cd /mnt/c/Users/User/OneDrive/1340/Project-Repo
