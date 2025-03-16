0653f464a5d89d99db7bc18246d5ab023c6cf109
Stevensun666 <u3628968@connect.hku.hk>
2025-03-16 23:37:20 +0800

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <conio.h>
 using namespace std;
-struct player
+int difficulty=0;
+struct Player{
+  string name;
+  int health;
+  int hungry;
+  int sanity;
+  string inventory;// vector<string>
+}
+Player p1={"player", 100, 100, 10};
+void display_player_status()
+{
+  cout << "---YOUR STATUS---"<< endl;
+  cout << "Name: " << p1.name << endl;
+  cout << "Health: " << p1.health << endl;
+  cout << "Hungry: " << p1.hungry << endl;
+  cout << "Sanity: " << p1.sanity << endl;
+  cout << "Inventory: "<< p1.inventory << endl;
+}
