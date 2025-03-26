8fd1b0d7ae956daa2e509c69f57791fb1433a470
AlonsoCltz <alonsosun2006@gmail.com>
2025-03-26 20:38:38 +0800

    large change in format, tested fine, makefile in src folder, use make run to test

--- src/exploration.cpp
+++ src/exploration.cpp
@@ -1,4 +1,10 @@
-#include "exploration.h"
-#include "game_state.h"
+#include "../include/exploration.h"
+#include "../include/game_state.h"
+#include "../include/map_head.h"
 
-void start_explore() { /* Exploration logic */ }
+void start_explore() { run_shelter(); }
+
+int main() {
+    start_explore();
+    return 0;
+}
